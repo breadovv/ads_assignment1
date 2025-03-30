33d0dfa1a586f204c32de4ff20014a31514e94e9
breadov <alikboat@gmail.com>
2025-03-30 21:01:36 +0500

    created 7 class , solved 7 problem

--- src/Problem7.java
+++ src/Problem7.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 public class Problem7 {
     public static void reversePrint(int n, Scanner sc) {
-        if (n == 0) return; // Base case: Stop recursion when all elements are read
+        if (n == 0) return;
 
-        long num = sc.nextLong(); // Read the number
-        reversePrint(n - 1, sc);  // Recursive call (next number is read before printing)
-        System.out.print(num + " "); // Print during backtracking (reverse order)
+        long num = sc.nextLong();
+        reversePrint(n - 1, sc);
+        System.out.print(num + " ");
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
